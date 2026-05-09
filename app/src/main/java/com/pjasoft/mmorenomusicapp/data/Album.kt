ee106c0893a054014e2f4fcbab699bf7ae20e7cd
majo101 <mml79670@lasallebajio.edu.mx>
2026-05-09 12:04:11 -0600

    Estructura base

---
+++ app/src/main/java/com/pjasoft/mmorenomusicapp/data/Album.kt
@@ -0,0 +1,12 @@
+package com.pjasoft.mmorenomusicapp.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Album(
+    val id: Int,
+    val title: String,
+    val artist: String,
+    val description: String? = null,
+    val image: String
+)
